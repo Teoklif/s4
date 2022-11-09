764da8dbc48a0d975ebdb09d9fe53ac6048fc5fd
alpryakh <alpryakh@gmail.com>
2022-11-09 20:19:02 +0300

    ex 25

---
+++ ex 25/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Write A:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write B:");
+int b = Convert.ToInt32(Console.ReadLine());
+double number = Math.Pow(a, b);
+Console.WriteLine(a + "^" + b +"="+number);
