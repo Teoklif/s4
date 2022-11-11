fa04b9b7ca013e824002a572eee0632d4358c1a2
alpryakh <alpryakh@gmail.com>
2022-11-11 18:53:06 +0300

    ex 27

--- ex 27/Program.cs
+++ ex 27/Program.cs
@@ -1,2 +1,17 @@
-﻿
+﻿Console.Clear();
+Console.Write("Enter number:");
 
+int num = int.Parse(Console.ReadLine()); 
+Console.WriteLine(Sum(num));
+int Sum(int number)
+{int sum = 0;
+for (int i = 10; i<number*10; i=10)
+{
+    sum += number%i;
+    number=number/10;
+   
+    
+
+}
+return sum;
+}
