b653b1795649016d10e947cb4cd2aedd1b7e3d59
alpryakh <alpryakh@gmail.com>
2022-11-11 19:06:46 +0300

    ex 29

---
+++ ex 29/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = GetArray(8);
+Console.Write($"Here it is: [ {String.Join(", ", array)} ]");
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        result[i]=new Random().Next();
+    }
+    return result;
+}
